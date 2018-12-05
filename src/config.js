bbefd47e27440502a167191fa4128c1252aa2e6f
LZ <laizhi727@126.com>
2018-12-05 17:09:30 +0800

    Change project name and layout

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 export default {
   appName: {
-    // full: '平安校园智慧通行',
-    full: 'LAdmin',
-    // subTitle: '电子科技大学',
-    subTitle: 'Material Design',
-    attr: 'LAdmin',
+    full: '平安校园智慧通行管理系统',
+    // full: 'LAdmin',
+    subTitle: '电子科技大学',
+    // subTitle: 'Material Design',
+    attr: '平安校园智慧通行',
 
     // full path
-    logoPath: '',
+    logoPath: '@/assets/uestc-main-normal.png',
   },
   permissionSeq: ':',
   navDrawerWidth: 300,
